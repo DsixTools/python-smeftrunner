ffd373c123aa20951ecbc08426bd73f3c0963ae3
David Straub <david.straub@tum.de>
2017-07-24 16:08:42 +0200

    Comments for unit tests

--- test.py
+++ test.py
@@ -4,21 +4,25 @@ import numpy.testing as npt
 from beta import beta
 import json
 
+# read in JSON files with numerical input & output of Mathematica code
 rpar = json.load(open('random_par.json', 'r'))
 rwc = json.load(open('random_wc.json', 'r'))
 betas_re = json.load(open('betas_re.json', 'r'))
 betas_im = json.load(open('betas_im.json', 'r'))
 
+# reconstruct SM parameters
 g, gp, gs, Lambda, m2 = rpar[:5]
 Gu = np.array(rpar[5]) + 1j*np.array(rpar[6])
 Gd = np.array(rpar[7]) + 1j*np.array(rpar[8])
 Ge = np.array(rpar[9]) + 1j*np.array(rpar[10])
 HIGHSCALE = 1000
 
+# names of WCs with 0, 2, or 4 fermions (i.e. scalars, 3x3 matrices, and 3x3x3x3 tensors)
 WC0 = ["G", "Gtilde", "W", "Wtilde", "CurlyPhi", "CurlyPhiEmptySquare", "CurlyPhiD", "CurlyPhiG", "CurlyPhiB", "CurlyPhiW", "CurlyPhiWB", "CurlyPhiGtilde", "CurlyPhiBtilde", "CurlyPhiWtilde", "CurlyPhiWtildeB"]
 WC2 = ["uCurlyPhi", "dCurlyPhi", "eCurlyPhi", "eW", "eB", "uG", "uW", "uB", "dG", "dW", "dB", "CurlyPhil1", "CurlyPhil3", "CurlyPhie", "CurlyPhiq1", "CurlyPhiq3", "CurlyPhiu", "CurlyPhid", "CurlyPhiud"]
 WC4 = ["ll", "qq1", "qq3", "lq1", "lq3", "ee", "uu", "dd", "eu", "ed", "ud1", "ud8", "le", "lu", "ld", "qe", "qu1", "qd1", "qu8", "qd8", "ledq", "quqd1", "quqd8", "lequ1", "lequ3", "duql", "qque", "qqql", "duue"]
 
+# construct numerical values of WCs
 WC = {}
 for i, n in enumerate(WC0):
     WC[n] = rwc[0][i]
@@ -28,8 +32,8 @@ for i, n in enumerate(WC4):
     WC[n] = np.array(rwc[2][i])
 
 class TestBeta(unittest.TestCase):
-
     def test_beta(self):
+        """check that numerical output of Python code equals Mathematica code"""
         my_beta = beta(g, gp, gs, m2, Lambda, Gu, Gd, Ge, HIGHSCALE, WC)
         for i, n in enumerate(WC0):
             self.assertAlmostEqual(betas_re[0][i]/my_beta[n].real, 1, places=4)

4104ad3ba67fa82dc58dbebbb1bcb370bd801e3e
David Straub <david.straub@tum.de>
2017-07-25 17:58:55 +0200

    Add Xuanyou to authors

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='smeftrunner',
       version='0.1',
-      author='David M. Straub',
-      author_email='david.straub@tum.de',
+      author='Xuanyou Pan, David M. Straub',
+      author_email='xuanyou.pan@tum.de, david.straub@tum.de',
       url='https://github.com/DavidMStraub/smeftrunner',
       description='A Python package for the renormalization group evolution in the Standard Model Effective Field Theory (SMEFT).',
       license='MIT',

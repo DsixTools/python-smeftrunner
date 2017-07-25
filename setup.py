6eb7e773a5a61aa74de0142fb9032934c1363e05
David Straub <david.straub@tum.de>
2017-07-25 18:02:56 +0200

    Add long description with credits to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,15 @@ setup(name='smeftrunner',
       author_email='xuanyou.pan@tum.de, david.straub@tum.de',
       url='https://github.com/DavidMStraub/smeftrunner',
       description='A Python package for the renormalization group evolution in the Standard Model Effective Field Theory (SMEFT).',
+      long_description="""A Python package for the renormalization group
+      evolution in the Standard Model Effective Field Theory (SMEFT).
+      Based on the SMEFTrunner module of the DSixTools Mathematica package
+      by Alejandro Celis, Javier Fuentes-Martín, Avelino Vicente,
+      and Javier Virto (arXiv:1704.04504). RGEs based on arXiv:1308.2627,
+      arXiv:1310.4838, and arXiv:1312.2014 by Rodrigo Alonso,
+      Elizabeth Jenkins, Aneesh Manohar, and Michael Trott."""
       license='MIT',
-      packages=['smeftrunner'],
+      packages=find_packages(),
       package_data={
       'smeftrunner':['tests/data/*',
               ]

0fa8200383271b4b7a7603d44649264ec07a4e06
David Straub <david.straub@tum.de>
2017-11-06 20:05:03 +0100

    Fix phase rotation and add unit tests

--- smeftrunner/definitions.py
+++ smeftrunner/definitions.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from cmath import phase, exp
+from math import asin, atan, pi, sin, cos
 
 I3 = np.identity(3)
 
@@ -1745,15 +1746,31 @@ def argdet(U):
     return np.angle(np.linalg.det(U))
 
 def mixing_phases(U):
-    """Return the CP phases of the CKM or PMNS matrix
+    """Return the angles and CP phases of the CKM or PMNS matrix
     in standard parametrization, starting from a matrix with arbitrary phase
     convention."""
     f = {}
+    # angles
+    f['t13'] = asin(abs(U[0,2]))
+    if U[0,0] == 0:
+        f['t12'] = pi/2
+    else:
+        f['t12'] = atan(abs(U[0,1])/abs(U[0,0]))
+    if U[2,2] == 0:
+        f['t23'] = pi/2
+    else:
+        f['t23'] = atan(abs(U[1,2])/abs(U[2,2]))
+    s12 = sin(f['t12'])
+    c12 = cos(f['t12'])
+    s13 = sin(f['t13'])
+    c13 = cos(f['t13'])
+    s23 = sin(f['t23'])
+    c23 = cos(f['t23'])
     # standard phase
-    if U[1,0]*U[1,2].conj() == 0:
+    if (s12*s23) == 0 or (c12*c13**2*c23*s13) == 0:
         f['delta'] = 0
     else:
-        f['delta'] = phase(-U[0,0]*U[0,2].conj()/(U[1,0]*U[1,2].conj()))
+        f['delta'] = -phase((U[0,0].conj()*U[0,2]*U[2,0]*U[2,2].conj()/(c12*c13**2*c23*s13) + c12*c23*s13)/(s12*s23))
     # Majorana phases
     f['delta1']  = phase(exp(1j*f['delta']) * U[0, 2])
     f['delta2']  = phase(U[1, 2])
--- smeftrunner/tests/test_def.py
+++ smeftrunner/tests/test_def.py
@@ -88,3 +88,64 @@ class TestSymm(unittest.TestCase):
                 self.assertNotEqual(C_num[k][e].imag, 0)
                 # symmetrized im parts ARE zero
                 self.assertEqual(C_num_symm[k][e].imag, 0)
+
+    def test_rephasing(self):
+        # some auxiliary functions
+        def diagonal_phase_matrix(a1, a2, a3):
+            r"""A diagonal $3\times 3$ matrix with $jj$-element $e^{i a_j}$"""
+            return np.diag(np.exp(1j*np.array([a1, a2, a3])))
+        # from flavio.physics.ckm
+        from math import sin, cos
+        from cmath import exp
+        def ckm_standard(t12, t13, t23, delta):
+            c12 = cos(t12)
+            c13 = cos(t13)
+            c23 = cos(t23)
+            s12 = sin(t12)
+            s13 = sin(t13)
+            s23 = sin(t23)
+            return np.array([[c12*c13,
+                c13*s12,
+                s13/exp(1j*delta)],
+                [-(c23*s12) - c12*exp(1j*delta)*s13*s23,
+                c12*c23 - exp(1j*delta)*s12*s13*s23,
+                c13*s23],
+                [-(c12*c23*exp(1j*delta)*s13) + s12*s23,
+                -(c23*exp(1j*delta)*s12*s13) - c12*s23,
+                c13*c23]])
+        # general CKM/PMNS-like matrix
+        def unitary_matrix(t12, t13, t23, delta, delta1, delta2, delta3, phi1, phi2):
+            ph1 = diagonal_phase_matrix(delta1, delta2, delta3)
+            ph2 = diagonal_phase_matrix(-phi1/2, -phi2/2, 0)
+            ckm = ckm_standard(t12, t13, t23, delta)
+            return np.dot(ph1, np.dot(ckm, ph2))
+        # a random unitary matrix
+        U = np.array([[-0.4825-0.5529j,  0.6076-0.0129j,  0.1177+0.2798j],
+                   [ 0.1227-0.5748j, -0.1685-0.5786j, -0.2349-0.486j ],
+                   [-0.0755+0.3322j,  0.5157+0.0393j, -0.7321-0.2837j]])
+        f = definitions.mixing_phases(U)
+        # check that matrix reconstructed from extracted angles and phases
+        # coincides with original matrix
+        npt.assert_array_almost_equal(U, unitary_matrix(**f),
+            decimal=4)
+        # pathological case: unit matrix. Check that everything is zero
+        f = definitions.mixing_phases(np.eye(3))
+        for k, v in f.items():
+            self.assertEqual(v, 0, msg='{} is not zero'.format(k))
+        # random mass matrices
+        Mu = np.array([[ 0.6126+0.9819j,  0.0165+0.3709j,  0.0114+0.7819j],
+            [ 0.6374+0.8631j,  0.1249+0.8346j,  0.6940+0.4495j],
+            [ 0.1652+0.9667j,  0.4952+0.1281j,  0.7719+0.2325j]])
+        Md = np.array([[ 0.2874+0.2065j,  0.2210+0.9077j,  0.4053+0.454j ],
+            [ 0.0339+0.8332j,  0.5286+0.6339j,  0.3142+0.113j ],
+            [ 0.9379+0.2952j,  0.8488+0.634j ,  0.1413+0.9175j]])
+        UuL, Su, UuR = definitions.msvd(Mu)
+        UdL, Sd, UdR = definitions.msvd(Md)
+        f = definitions.mixing_phases(UuL.conj().T @ UdL)
+        UuL_, UdL_, UuR_, UdR_ = definitions.rephase_standard(UuL, UdL, UuR, UdR)
+        # rephased CKM
+        K = UuL_.conj().T @ UdL_
+        # CKM in standard parametrization
+        K_std = unitary_matrix(f['t12'], f['t13'], f['t23'], f['delta'], 0, 0, 0, 0, 0)
+        # ... should be equal!
+        npt.assert_array_almost_equal(K, K_std, decimal=10)
